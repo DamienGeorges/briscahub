941325daab6a8621acd579f91391c59c3e7c970b
unknown <dami@st-d02634.ad.nfit.au.dk>
2015-10-13 13:37:10 +0200

    compute convex hull for our species

--- scripts/0_data_formatting/shrubs_convex_hull_computation.R
+++ scripts/0_data_formatting/shrubs_convex_hull_computation.R
@@ -12,13 +12,38 @@
 
 ## -- Start of script ----------------------------------------------------------
 rm(list = ls())
-path.to.briscahub <- "~/Work/BRISCA/briscahub" ## path to teh dir where vbrscahub repos have been cloned
-path.to.full.occ.dir <- "" ## path to the dir where all occurences for scpecies are stored
-out.dir <- "~/Work/BRISCA/workdir/brsica_shrubs_convex_hull"
+
+## -- decine some paths depending on the machine used --------------------------
+# ## on pinea
+# path.to.briscahub <- "~/Work/BRISCA/briscahub" ## path to teh dir where vbrscahub repos have been cloned
+# out.dir <- "~/Work/BRISCA/workdir/brsica_shrubs_convex_hull"
+## on brsica
+path.to.briscahub <- "J:/People/Damien/BRISCA/briscahub" ## path to teh dir where vbrscahub repos have been cloned
+out.dir <- "J:/People/Damien/BRISCA/workdir/brsica_shrubs_convex_hull"
+
+path.to.full.occ.dir <- "I:/C_Write/Signe/aa_BRISCA/Data/StudySpecies/Processed/Species.list/Occurrence.tables.combined.all.sources/" ## path to the dir where all occurences for scpecies are stored
 dir.create(out.dir, showWarnings = FALSE, recursive = TRUE)
 
-## load IUCN_eval Gilles' function
-source(file.path(path.to.briscahub, "scripts", "0_data_formatting", "IUCN_eval.r"))
+## -- require libraries needed -------------------------------------------------
+library(sp)
+library(grDevice)
+
+## -- load IUCN_eval Gilles' function ------------------------------------------
+# source(file.path(path.to.briscahub, "scripts", "0_data_formatting", "IUCN_eval.r"))
+
+## -- define the convex hull polygon function ----------------------------------
+##' @note this function comes from Gilles D. function
+Convex_Hull_Poly <- function(XY){
+  hpts <- chull(XY); hpts <- c(hpts, hpts[1])
+  POLY <- "POLYGON(("
+  for (i in 1:length(hpts)){
+    POLY <- paste(POLY,XY[hpts[i],2]," ",XY[hpts[i],1], sep="")
+    if(i!=length(hpts)) POLY <- paste(POLY,", ", sep="")
+    if(i==length(hpts)) POLY <- paste(POLY,"))", sep="")
+  }
+  p1 = readWKT(POLY)  
+  return(p1)
+}
 
 ## -- load the reference data --------------------------------------------------
 sp.dat <- read.table(file.path(path.to.briscahub, "data/sp.list_08102015_red.txt"), 
@@ -28,39 +53,16 @@ sp.list <- unique(sp.dat$Genus.species)
 
 ## loop over species 
 for(spe in sp.list){
-  occ.dat <- read.table()
-  
+  cat("\n> spe:", spe)
+  ## load the table where all the species occurences are recorded
+  occ.dat <- read.table(file.path(path.to.full.occ.dir, paste0(spe, ".txt")),
+                        sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+  occ.dat <- na.omit(occ.dat)
+  ## occurences are defines in laea proj system
+  crs.laea <- CRS("+proj=laea +lat_0=90 +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs")
+  ## compute the convex hull for our species
+  sp.ch.poly <- Convex_Hull_Poly(occ.dat[, c("X", "Y")])
+  proj4string(sp.ch.poly) <- crs.laea
+  ## save the object as an R object
+  save(sp.ch.poly, file = file.path(out.dir, paste0(spe, "_convex_hull_poly.RData")))
 }
-
-
-
-
-# 
-# ### Examples of using IUCN_eval function
-# ### Drawing the map.
-# IUCN_eval(DATA, country_map=africa, Cell_size=2, DrawMap=T)
-# ### A pdf file should be created in the working directory
-# 
-# IUCN_eval(DATA, country_map=africa, Cell_size=2, DrawMap=T, add.legend=T)
-# 
-# 
-# ### Drawing the map witout adding the legend
-# IUCN <- IUCN_eval(DATA, Cell_size=2, DrawMap=F, add.legend=F)
-# p1 <- IUCN[[2]]
-# 
-# #### You add a buffer of width to your convex hull
-# p1_Buffered <- gBuffer(p1[[1]], width = 5)
-# 
-# 
-# ### You can mask the predictors according to the convex_hull+buffer
-# mask(x = z[[i]], mask = p1_Buffered)
-# 
-# 
-# 
-# 
-
-
-
-
-
-
