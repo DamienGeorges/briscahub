5f47eea1a2459a25241ec3cc8f7658bc01478dc0
DamienGeorges <dami@st-d02634.ad.nfit.au.dk>
2016-11-06 18:40:42 +0100

    create scripts to compute alphadiv stats and produce associated graphs fig1b

---
+++ scripts/4_results_analysis/alphadiv_stat_plot.R
@@ -0,0 +1,162 @@
+##' ---
+##' @title Plot the alpha diversity summaries
+##' @descrtiption This script has been designed to produce geographical outputs
+##' @author damien georges
+##' @date 2016-11-05
+##' @licence GPL-2
+##' ---
+
+##' ## Initialisation
+
+rm(list = ls())
+
+## load needed libraries
+.libPaths("J:/People/Damien/RLIBS") ## on brisca cluster
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+## define the main paths to data
+briscahub.dir <- "J:/People/Damien/BRISCA/briscahub/" ## on brisca cluster
+alphadiv.tab.path <- "I://C_Write/Damien/BRISCA/backup_idiv_cluster/SRC_baseline_alpha_and_turnover_stack_by_growth_form_stat/mean_rcp_gcm_alphadiv_ras/gg.bp.stat.RData"
+out.dir.path <-"I:/C_Write/Damien/BRISCA/figures/2016-10-27"
+
+dir.create(out.dir.path, recursive = TRUE, showWarnings = FALSE)
+
+##' ## get and reshape the data
+
+## laod alphadiv scores table
+alphadiv.tab <- get(load(alphadiv.tab.path))
+
+## load species ref table
+sp.tab <- read.table(file.path(briscahub.dir, "data/sp.list_08102015_red.txt"),
+                     sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+sp.tab <- sp.tab[ sp.tab$Growth.form.height == 'SHRUB', ]
+
+##' ## produce some graphs
+
+## define an article fig friendly theme
+gg.theme <- theme(axis.text.x = element_text(angle = 20, hjust = 1, vjust = 1),
+                  panel.grid.major = element_line(colour = "grey90"),
+                  panel.grid.minor = element_blank(), #element_line(colour = "grey80"),
+                  panel.border = element_rect(fill = NA, colour = 'grey80'),
+                  panel.background = element_rect(fill = NA),
+                  strip.background = element_rect(fill = NA, colour = 'grey80'),
+                  legend.background = element_blank()) 
+
+gg.dat <- alphadiv.tab %>% mutate(metric.name = metric) %>% 
+  filter(area %in% c("r.from.sa", "r.sa", "r.la", "r.ha"),
+         metric.name %in% c("alphadiv.change", "gain", "lost"))
+
+gg.dat$area <- factor(gg.dat$area, levels =  c("r.from.sa", "r.sa", "r.la", "r.ha"), labels = c("Full arctic", "Sub-arctic", "Low arctic", "High arctic"))
+gg.dat$metric.name = factor(gg.dat$metric.name, levels = c("alphadiv.change", "gain", "lost"), labels = c("AlphaDiv Change", "Nb Species Gained", "Nb Species Lost"))
+
+## check the number of combination computed
+gg.dat %>% ungroup %>% group_by(biotic.inter, dispersal.filter) %>% summarise(n = n())
+## fig1b asked by Anne and Signe on Oct 3
+
+## reshape the table to produce the new graph
+# 
+# gg.dat.t.test_ <- xx %>% ungroup %>% select(rcp, gcm, species, scenario.biomod, dispersal.filter, biotic.inter, metric.name, area, metric.val) %>%
+#   group_by(metric.name, area) %>% mutate(dispersal.and.biotic = factor(paste0(dispersal.filter, "_", biotic.inter))) %>%
+#   do(data.frame(try(pairwise.wilcox.test(.$metric.val, .$dispersal.and.biotic, paired = TRUE)$p.value %>% data.frame %>% 
+#                   mutate(var1.name = rownames(.)) %>% gather(var2.name, p.value, - var1.name))))
+# 
+# 
+# 
+# gg.dat.t.test_ %>% ungroup %>% group_by(area, metric.name) %>% summarise(n = sum(is.finite(p.value)))
+# gg.dat.t.test_ %>% filter(p.value > 0.05) %>% View
+# 
+# xxxx <- xx %>% filter(area == "Full arctic", metric.name =="SR Gain (%)")  %>%
+#   mutate(dispersal.and.biotic = factor(paste0(dispersal.filter, "_", biotic.inter)))
+# table(na.omit(xxxx$dispersal.and.biotic))
+
+# pairwise.wilcox.test(xxxx$metric.val, xxxx$dispersal.and.biotic, paired = TRUE)$p.value %>% data.frame %>% 
+#   mutate(var1.name = rownames(.)) %>% gather(var2.name, p.value, - var1.name)
+# gg.plot <- ggplot(gg.dat, aes(1, metric.val, fill = dispersal.filter, linetype = biotic.inter)) +
+#   geom_boxplot(outlier.colour = NA) + facet_grid(metric.name ~ area, scale = 'free_y') + 
+#   coord_cartesian(ylim = c(-100, 4500)) + ## with same baselines or not change graph scale
+#   scale_fill_brewer(palette = "Blues", guide = guide_legend(title = "Dispersal distance")) + 
+#   scale_linetype_discrete(guide = guide_legend(title = "Biotic interactions")) +
+#   xlab("") + ylab("") +
+#   gg.theme + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+# gg.plot
+
+gg.plot <- ggplot(gg.dat, aes(1, fill = dispersal.filter, linetype = biotic.inter)) +
+  geom_boxplot(aes(lower = lower, middle = middle, upper = upper, ymin = ymin, ymax = ymax), 
+               stat = "identity", outlier.colour = NA, position = position_dodge(1.5)) + 
+  facet_grid(metric.name ~ area, scale = 'free_y') + 
+  scale_fill_brewer(palette = "Blues", guide = guide_legend(title = "Dispersal distance")) + 
+  scale_linetype_discrete(guide = guide_legend(title = "Biotic interactions")) +
+  xlab("") + ylab("") +
+  gg.theme + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+gg.plot
+
+
+ggsave(file.path(out.dir.path, "fig1b2.png"), gg.plot, width = 297, height = 210, units = 'mm')
+
+## try to replace the alpha.div change by the raw alphadiv
+gg.dat2 <- alphadiv.tab %>% mutate(metric.name = metric) %>% 
+  filter(area %in% c("r.from.sa", "r.sa", "r.la", "r.ha"),
+         metric.name %in% c("alphadiv", "gain", "lost"))
+
+gg.dat2$area <- factor(gg.dat2$area, levels =  c("r.from.sa", "r.sa", "r.la", "r.ha"), labels = c("Full arctic", "Sub-arctic", "Low arctic", "High arctic"))
+gg.dat2$metric.name = factor(gg.dat2$metric.name, levels = c("alphadiv", "gain", "lost"), labels = c("AlphaDiv", "Nb Species Gained", "Nb Species Lost"))
+
+
+
+gg.plot <- ggplot(gg.dat2, aes(1, fill = dispersal.filter, linetype = biotic.inter)) +
+  geom_boxplot(aes(lower = lower, middle = middle, upper = upper, ymin = ymin, ymax = ymax), 
+               stat = "identity", outlier.colour = NA, position = position_dodge(1.5)) + 
+  facet_grid(metric.name ~ area, scale = 'free_y') + 
+  scale_fill_brewer(palette = "Blues", guide = guide_legend(title = "Dispersal distance")) + 
+  scale_linetype_discrete(guide = guide_legend(title = "Biotic interactions")) +
+  xlab("") + ylab("") +
+  gg.theme + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+gg.plot
+
+
+ggsave(file.path(out.dir.path, "fig1c.png"), gg.plot, width = 297, height = 210, units = 'mm')
+
+## try to replace the alpha.div change by the raw alphadiv
+gg.dat2 <- alphadiv.tab %>% mutate(metric.name = metric) %>% 
+  filter(area %in% c("r.from.sa", "r.sa", "r.la", "r.ha"),
+         metric.name %in% c("alphadiv.change", "pc.gain", "pc.lost"))
+
+gg.dat2$area <- factor(gg.dat2$area, levels =  c("r.from.sa", "r.sa", "r.la", "r.ha"), labels = c("Full arctic", "Sub-arctic", "Low arctic", "High arctic"))
+gg.dat2$metric.name = factor(gg.dat2$metric.name, levels = c("alphadiv.change", "pc.gain", "pc.lost"), labels = c("AlphaDiv Change", "Species Gained (%)", "Species Lost (%)"))
+
+
+
+gg.plot <- ggplot(gg.dat2, aes(1, fill = dispersal.filter, linetype = biotic.inter)) +
+  geom_boxplot(aes(lower = lower, middle = middle, upper = upper, ymin = ymin, ymax = ymax), 
+               stat = "identity", outlier.colour = NA, position = position_dodge(1.5)) + 
+  facet_grid(metric.name ~ area, scale = 'free_y') + 
+  scale_fill_brewer(palette = "Blues", guide = guide_legend(title = "Dispersal distance")) + 
+  scale_linetype_discrete(guide = guide_legend(title = "Biotic interactions")) +
+  xlab("") + ylab("") +
+  gg.theme + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+gg.plot
+
+
+ggsave(file.path(out.dir.path, "fig1b.png"), gg.plot, width = 297, height = 210, units = 'mm')
+
+# ### make a try with a semilog scale
+# ## to deal with the boxplot outliers
+# 
+# gg.dat.log.no.ol <- gg.dat %>% group_by(dispersal.filter, biotic.inter, metric.name, area) %>%
+#   do(data.frame(t(boxplot.stats(log((.$metric.val + 100) / 100))$stats)))
+# 
+# gg.plot <- ggplot(gg.dat.log.no.ol, aes(1, fill = dispersal.filter, linetype = biotic.inter)) +
+#   geom_boxplot(aes(lower = X2, middle = X3, upper = X4, ymin = X1, ymax = X5), 
+#                stat = "identity", outlier.colour = NA, position = position_dodge(1.5)) + 
+#   facet_grid(metric.name ~ area, scale = 'free_y') + 
+#   scale_fill_brewer(palette = "Blues", guide = guide_legend(title = "Dispersal distance")) + 
+#   scale_linetype_discrete(guide = guide_legend(title = "Biotic interactions")) +
+#   xlab("") + ylab("") +
+#   gg.theme + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())
+# gg.plot
+# 
+# ggsave(file.path(out.dir.path, "fig1_pseudo_log.png"), gg.plot, width = 297, height = 210, units = 'mm')
+# 
+
---
+++ scripts/4_results_analysis/recompute_alphadiv_stats_table_by_area.R
@@ -0,0 +1,149 @@
+##' ---
+##' @title Construct the alpha diversity, gain and loss summary tables to 
+##'   construct the article boxplots (fig2)
+##' @author damien georges
+##' @date 2016-05-11
+##' @licence GPL-2
+##' ---
+
+##' **note: this script has to be run after alphadiv_gain_and_loss_maps.R**
+
+##' ## Initialisation
+
+rm(list = ls())
+
+## set some parameters
+same.baseline <- TRUE ## do we consider the same baseline (climate filtered no dispersal) as a baseline or 
+## each scenario current prediction as baseline
+machine <- "signe_cluster" # "sdiv" ## the name of the machine the script will run on
+n.cores <- 1 ## number of resuired cores
+
+## define the main paths to data
+if(machine == "leca97"){
+} else if (machine == "pinea"){
+} else if (machine == "sdiv"){
+} else if (machine == "signe_cluster"){
+  .libPaths( "J:/People/Damien/RLIBS")
+  briscahub.dir <- "J://People/Damien/BRISCA/briscahub/"
+  alpha.div.map.path <-  paste0("I://C_Write/Damien/BRISCA/backup_idiv_cluster/", ifelse(same.baseline, "SRC_baseline", "SRC"), "_alpha_and_turnover_stack_by_growth_form")
+  param.tab.path <- paste0("I://C_Write/Damien/BRISCA/backup_idiv_cluster/", ifelse(same.baseline, "SRC_baseline", "SRC"), "_alpha_and_turnover_stack_by_growth_form/gg.calc.RData")
+  out.dir.path <- paste0("I://C_Write/Damien/BRISCA/backup_idiv_cluster/", ifelse(same.baseline, "SRC_baseline", "SRC"), "_alpha_and_turnover_stack_by_growth_form_stat")
+} else stop("\n> unknow machine!")
+
+setwd(file.path(briscahub.dir, "workdir"))
+## load needed libraries
+library(raster)
+library(dplyr)
+library(multidplyr)
+library(parallel)
+library(ggplot2)
+library(tidyr)
+
+dir.create(out.dir.path, showWarnings = FALSE, recursive =TRUE)
+
+
+## load couple of masks to compute stats locally
+r.full.area <- raster("I://C_Write/Damien/BRISCA/data/mask_raster_arctic_area_2016-08-22/mask_full_area_no_ice.grd")
+r.from.sa <- raster("I://C_Write/Damien/BRISCA/data/mask_raster_arctic_area_2016-08-22/mask_from_subarctic_area_no_ice.grd")
+r.sa <- raster("I://C_Write/Damien/BRISCA/data/mask_raster_arctic_area_2016-08-22/mask_subarctic_area_no_ice.grd")
+r.from.la <- raster("I://C_Write/Damien/BRISCA/data/mask_raster_arctic_area_2016-08-22/mask_from_lowarctic_area_no_ice.grd")
+r.la <- raster("I://C_Write/Damien/BRISCA/data/mask_raster_arctic_area_2016-08-22/mask_lowarctic_area_no_ice.grd")
+r.ha <- raster("I://C_Write/Damien/BRISCA/data/mask_raster_arctic_area_2016-08-22/mask_higharctic_area_no_ice.grd")
+
+mask.ids <- c('r.full.area', 'r.from.sa', 'r.sa', 'r.from.la', 'r.la', 'r.ha')
+
+## load the formal alphadiv ref tab
+load(param.tab.path)
+# ## add the region field
+# gg.calc <- gg.calc %>% mutate(stack.file.name = unlist(stack.file.name)) %>% 
+#   data.frame %>% cbind(area = rep(mask.ids, each = nrow(gg.calc)))
+
+## We will aggregate resutls accross GCMs and RCPs
+mean.alpha.div.dir <- file.path(out.dir.path, "mean_rcp_gcm_alphadiv_ras")
+dir.create(mean.alpha.div.dir, showWarnings = FALSE, recursive = TRUE)
+
+## test
+x <- gg.calc %>% filter(scenario.biomod == "pure_climate",
+                        biotic.inter == "no", 
+                        dispersal.filter == "unlimited",
+                        growth.form == "Dwarf Shrubs"#,
+                        # area == "r.full.area"
+                        )
+## end test
+mean_alphadiv <- function(x){
+  scenario.biomod_ <- unique(x$scenario.biomod)
+  biotic.inter_ <- unique(x$biotic.inter)
+  dispersal.filter_ <- unique(x$dispersal.filter)
+  growth.form_ <- unique(x$growth.form)
+  # area_ <- unique(x$area)
+  stk.files_ <- x$stack.file.name
+  n.stk_ <- length(stk.files_)
+  stk.name_ <- file.path(mean.alpha.div.dir, paste0("mean_alphadiv_stk_across_rcp_gcm__", scenario.biomod_, "__", biotic.inter_, "__", dispersal.filter_, "__", growth.form_, ".grd"))
+  stk.list_ <- lapply(stk.files_, stack, RAT = FALSE)
+  # stk.list_$fun <- mean
+  # stk.list_$filename <- stk.name_
+  # stk.mean_ <- do.call(overlay, tail(stk.list_, 5)) 
+  stk.sum_ <- stk.list_[[1]]
+  for(l in 2:n.stk_) stk.sum_ <- stk.sum_ + stk.list_[[l]]
+  stk.mean_ <- stk.sum_ / n.stk_
+  writeRaster(stk.mean_, filename = stk.name_, overwrite = TRUE)
+  return(data.frame(stk.name = stk.name_))
+}
+
+## do the mean of stack across gcm and rcps
+gg.mean.alphadiv <- gg.calc %>% ungroup %>% group_by(scenario.biomod, biotic.inter, dispersal.filter, growth.form) %>%
+  do(data.frame(mean_alphadiv(.)))
+
+save(gg.mean.alphadiv, file = file.path(mean.alpha.div.dir, "gg.mean.alphadiv.RData"))
+
+## compute the pix stats by area
+## test
+x <- gg.mean.alphadiv %>% filter(scenario.biomod == "pure_climate",
+                                 biotic.inter == "no",
+                                 dispersal.filter == "unlimited")
+## end test
+bp_stat_alphadiv <- function(x){
+  scenario.biomod_ <- unique(x$scenario.biomod)
+  biotic.inter_ <- unique(x$biotic.inter)
+  dispersal.filter_ <- unique(x$dispersal.filter)
+  ## do the sum of alphadiv, gain and loss maps (note: sum of turnover has no sens here)
+  stk.files_ <- x$stk.name
+  n.stk_ <- length(stk.files_)
+  stk.list_ <- lapply(stk.files_, stack, RAT = FALSE)
+  ## add the alphadiv.change layer
+  stk.list_ <- lapply(stk.list_, function(s_){ s_[['alphadiv.change']] <-  s_[['gain']] - s_[['lost']]; return(s_)})
+  ## add the %loss and %gain layers
+  stk.list_ <- lapply(stk.list_, function(s_){ 
+    cur.alpha_ <- s_[['alphadiv']] - s_[['alphadiv.change']]
+    s_[['pc.gain']] <-  (s_[['gain']] / cur.alpha_) * 100
+    s_[['pc.lost']] <-  (s_[['lost']] / cur.alpha_) * 100
+    return(s_)})
+  stk.sum_ <- stk.list_[[1]]
+  for(l in 2:n.stk_) stk.sum_ <- stk.sum_ + stk.list_[[l]]
+  ## compute the boxplot stats by area
+  out.list <- vector(length = length(mask.ids), mode = 'list')
+  names(out.list) <- mask.ids
+  for(m_ in names(out.list)){ ## test m_ <- "r.sa"
+    stk.tmp_ <- mask(stk.sum_, get(m_))
+    bp_stat_ <- boxplot(stk.tmp_, na.rm = TRUE)
+    out_tab_ <- data.frame(t(bp_stat_$stats))
+    colnames(out_tab_) <- c("ymin", "lower", "middle", "upper", "ymax")
+    out_tab_$area <- m_
+    out_tab_$n <- bp_stat_$n
+    out_tab_$metric <- bp_stat_$names
+    ## remove the turnover that has no sens here
+    out_tab_ <- out_tab_ %>% filter(metric != "turnover")
+    out.list[[m_]] <- out_tab_
+  }
+  out.tab <- bind_rows(out.list)
+  return(out.tab)
+}
+
+gg.bp.stat <- gg.mean.alphadiv %>% ungroup %>% group_by(scenario.biomod, biotic.inter, dispersal.filter) %>%
+  do(data.frame(bp_stat_alphadiv(.)))
+
+gg.bp.stat %>% data.frame %>% head
+save(gg.bp.stat, file = file.path(mean.alpha.div.dir, "gg.bp.stat.RData"))
+
+
+
